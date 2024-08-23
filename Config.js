697d4a345a7d1246b6d53a77719a7bd8dd691eb7
Avins-MD <kingsrobin254@gmail.com>
2024-08-23 17:17:35 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,18 +2,18 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['254745936840']
-global.ownername = "KISH"//owner name
+global.ownernumber = ['254713972753']
+global.ownername = "Avins"//owner name
 global.ytname = "YT: Kish-MD"
 global.socialm = "GitHub: Kish-MD"
 global.location = "Kenya"
 
-global.botname = 'KISH-MD' //name of the bot
+global.botname = 'Avins-MD' //name of the bot
 
 //sticker details
-global.stickername = 'KISH-MD'
+global.stickername = 'Avins-MD'
 global.packname = 'Sticker By'
-global.author = 'Kish Bot'
+global.author = '‌🇦‌𝐕🄸₦🆂-𝐌D҉̣͍̓̎͗͜͜'
 //console view/theme
 global.themeemoji = '𓅃'
 global.wm = "Kish botz inc."
